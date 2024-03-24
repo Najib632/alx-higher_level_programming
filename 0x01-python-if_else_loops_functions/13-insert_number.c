3e211a2c94a3b45a35a1a1d78fcd602ebc4cc34a
Najib Muhammad <najibm38@mail.com>
2024-03-24 11:46:27 +0100

    Bug fix: Handle at insertion at starting and ending

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,20 +13,37 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *curr = NULL;
 	listint_t *node = NULL;
 
-	if (head == NULL)
-		return (NULL);
+	if (*head == NULL)
+	{
+		node = malloc(sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = number;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
 	h = *head;
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
 	while (h != NULL)
 	{
 		curr = h->next;
-		if (h->n < number && curr->n > number)
+		if (curr == NULL || (curr != NULL && number < curr->n))
 		{
-			node = malloc(sizeof(listint_t));
-			if (node == NULL)
-				return (NULL);
-			node->n = number;
-			h->next = node;
-			node->next = curr;
+			if (number < h->n)
+			{
+				node->next = h;
+				*head = node;
+			}
+			else
+			{
+				h->next = node;
+				node->next = curr;
+			}
+			break;
 		}
 		h = h->next;
 	}
