c7639c319cb5e63d49a29d35990e09bd0de9778d
Najib Muhammad <najibm38@mail.com>
2024-04-08 10:18:49 +0100

    Rewrite file again

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
 if __name__ == "__main__":
-    from caculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
 
-a = 10
-b = 5
-
-result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
-result = sub(a, b)
-print("{:d} - {:d} = {:d}".format(a, b, result))
-result = mul(a, b)
-print("{:d} * {:d} = {:d}".format(a, b, result))
-result = div(a, b)
-print("{:d} / {:d} = {:d}".format(a, b, result))
+    print('{} {} {} = {}'.format(a, '+', b, add(a, b)))
+    print('{} {} {} = {}'.format(a, '-', b, sub(a, b)))
+    print('{} {} {} = {}'.format(a, '*', b, mul(a, b)))
+    print('{} {} {} = {}'.format(a, '/', b, div(a, b)))
