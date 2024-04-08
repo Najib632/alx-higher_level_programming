b6b58d210312a29e0b2593820808aaef44ff6342
Najib Muhammad <najibm38@mail.com>
2024-04-08 10:22:06 +0100

    Why didn't this work

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
 if __name__ == "__main__":
-    a = 10
-    b = 5
+    from calculator_1 import add, sub, mul, div
 
-    print('{} {} {} = {}'.format(a, '+', b, add(a, b)))
-    print('{} {} {} = {}'.format(a, '-', b, sub(a, b)))
-    print('{} {} {} = {}'.format(a, '*', b, mul(a, b)))
-    print('{} {} {} = {}'.format(a, '/', b, div(a, b)))
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, result_add))
+print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+print("{:d} / {:d} = {:d}".format(a, b, result_div))
